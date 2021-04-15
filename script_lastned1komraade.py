ba2f24e79c5683f1e3ca7e64ea250139ecca1e1b
LtGlahn <jensen72@gmail.com>
2021-04-15 16:11:00 +0200

    Bedre doc

--- script_lastned1komraade.py
+++ script_lastned1komraade.py
@@ -2,11 +2,13 @@
 Script som laster ned fire rapporttyper fra NVDB rapporter API til angitt mappe
 
 Rapporttyper som lastes ned: 
-    V1_Vegnettsrapport
-    V2_AggregertMengdePerVegkategori
-    V3_AggregertMengdePerVegnr
-    V4_Detaljert_mengde
-    Tilstandsrapport
+    V1_Vegnettsrapport.xlsx
+    V2_AggregertMengdePerVegkategori.xlsx
+    V3_AggregertMengdePerVegnr.xlsx
+    V4_Detaljert_mengde.xlsx
+    Tilstandsrapport.xlsx
+
+Disse lagres i mappen <mappenavn>_<Navn kontraktsområde>
 
 Dokumentasjon rapporter API 
 https://nvdbrapportapi.atlas.vegvesen.no/swagger-ui/#/ 
@@ -21,5 +23,6 @@ if __name__ == "__main__":
 
     kontrakter = [ '9305 Sunnfjord 2021-2026' ]
 
+    # Med mappenavn = 'rapportNedlasting' havner i mappen rapportNedlasting_9305_Sunnfjord_2021-2026/
     lastned.lastnedFlere( driftskontrakter=kontrakter, miljo='PROD', mappenavn='rapportNedlasting')
     
